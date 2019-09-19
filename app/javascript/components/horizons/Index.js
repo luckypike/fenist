6f4e5ceeac2d3691b654a810ba5804fd223d08de
Maxim <max.spiridonov@gmail.com>
2019-09-19 10:00:34 +0300

    Добавлен фест 2019

--- app/javascript/components/horizons/Index.js
+++ app/javascript/components/horizons/Index.js
@@ -1,8 +1,117 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import axios from 'axios'
+import { path } from '../Routes'
+import styles from './Index.module.css'
+import fish from '../../video/fish.mp4'
+import Section from './Section'
 
-export default function Index () {
+export default function Index (props) {
+  const [scrolling, setScrolling] = useState(false)
+  const [sections, setSections] = useState()
+
+  useEffect(() => {
+  const _onScroll = e => {
+    if (window.scrollY > (window.innerWidth / 1.7 - 64)) {
+      setScrolling(true)
+    } else if (window.scrollY < (window.innerWidth / 1.7 - 64)) {
+      setScrolling(false)
+    }
+  }
+    if (window) {
+      window.addEventListener('scroll', _onScroll)
+    }
+
+    const _loadAsyncData = async () => {
+      const { data: { sections } } = await axios.get(`${props.routes.fest_path}.json`)
+
+      setSections(sections)
+    }
+
+  _loadAsyncData()
+
+  }, [scrolling])
+
+  function handleScrollDown () {
+    if (document) {
+      window.scroll({ top: 1300, left: 0, behavior: 'smooth' });
+    }
+  }
+
   return (
-    <header>Index</header>
+    <div className={styles.root}>
+      <div className={classNames({[styles.placeholder]: scrolling})}></div>
+
+      <div className={styles.video}>
+        <video playsInline autoPlay loop muted>
+          <source src={fish} type="video/mp4"></source>
+        </video>
+      </div>
+
+      <section className={styles.screen}>
+        <div className={styles.preview}>
+          <div className={styles.date}>
+            <span>23-29 сентября</span>
+          </div>
+
+          <div className={styles.title}>
+            Фестиваль наук, искусств и технологий Фенист 2019
+          </div>
+
+          <div className={styles.description}>
+            3 дня открытого лектория в «Арсенале»  и неделя ежедневных дневных лекции в университетах и школах города ля школьников и студентов, учителей ипреподавателей.
+          </div>
+        </div>
+
+        <div className={styles.socials}>
+          <div className={styles.subscribe}>Подписывайтесь</div>
+          <a href="//vk.com/fenist" target="_blank" className={classNames(styles.social, styles.vk)}></a>
+          <a href="//fb.com/fenistnn" target="_blank" className={classNames(styles.social, styles.fb)}></a>
+          <a href="//twitter.com/fenistnn" target="_blank" className={classNames(styles.social, styles.tw)}></a>
+          <a href="//www.instagram.com/fenistnn/" target="_blank" className={classNames(styles.social, styles.inst)}></a>
+          <a href="//www.youtube.com/channel/UCni93LSiaWCThIWFdLvyb6A" target="_blank" className={classNames(styles.social, styles.yt)}></a>
+        </div>
+
+        <div className={styles.scroll} onClick={handleScrollDown}>
+          <div className={styles.program}>Программа фестиваля</div>
+          <div className={styles.arrow}>
+          </div>
+        </div>
+      </section>
+
+        <div className={styles.container}>
+          <div className={styles.group}>
+            <div className={styles.lecture}>
+              <span>Лекция</span>
+            </div>
+
+            <div className={styles.name}>
+              <span>Александр Соколов</span>
+            </div>
+
+            <div className={styles.text}>
+              Российский научный журналист и популяризатор науки. Основатель и главный редактор научно- просветительского портала Антропогенез.ру
+            </div>
+
+            <div className={styles.start}>
+              27 сентября, начало в 17:00, <br /> Арсенал, Кремль, стр. 6
+            </div>
+          </div>
+          <div className={styles.photo}></div>
+        </div>
+
+        <div className={styles.container}>
+          <div className={styles.events}>
+            {sections &&
+              <div>
+                {sections.map(section =>
+                  <Section section={section} key={section.id} />
+                )}
+              </div>
+            }
+          </div>
+        </div>
+    </div>
   )
 }
---
+++ app/javascript/components/horizons/Section.js
@@ -0,0 +1,145 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import * as moment from 'moment';
+import 'moment/locale/ru';
+moment.locale('ru');
+import classNames from 'classnames';
+
+import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+
+import { withRoutes } from '../Context/RoutesContext';
+
+import styles from './Section.module.css';
+
+class Section extends React.Component {
+  state = {
+    active: null,
+    open: false,
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.started_at = this.props.section.started_at ? moment.utc(this.props.section.started_at) : null;
+    this.ended_at = this.props.section.ended_at ? moment.utc(this.props.section.ended_at) : null;
+
+    this.days = [];
+    for (let m = moment(this.started_at); m.isBefore(this.ended_at); m.add(1, 'days')) {
+      this.days.push(moment(m));
+    }
+  }
+
+  componentDidMount() {
+    if(moment.utc().isBetween(this.started_at, this.ended_at)) {
+      this.setState({
+        active: moment.utc()
+      });
+    } else if(this.started_at) {
+      this.setState({
+        active: moment(this.started_at)
+      });
+    }
+  }
+
+  handleDayClick = (day) =>  {
+    this.setState({
+      active: moment(day),
+    });
+  }
+
+  handleToggleClick = () => {
+    this.setState(prevState => ({
+      open: !prevState.open
+    }));
+  }
+
+  render () {
+    const { section } = this.props;
+    const { active } = this.state;
+
+    if(!active) return null;
+
+    let events = section.events.filter(event => moment.utc(event.started_at).isSame(active, 'day'));
+    const eventsCount = events.length;
+    if(!this.state.open) {
+      events = events.slice(0, 10);
+    }
+
+
+    return (
+      <div className={classNames(styles.section, styles[`section_${section.id}`])}>
+        <h2>
+          {section.title}
+        </h2>
+
+        <div className={styles.days}>
+          {this.days.map((day, _) =>
+            <div className={classNames(styles.day, { [styles.active]: day.isSame(active, 'day') })} key={_} onClick={() => this.handleDayClick(day)}>
+              <span className={styles.short}>
+                {day.format('DD.MM')}
+              </span>
+              <span className={styles.long}>
+                {day.format('DD MMMM')}
+              </span>
+            </div>
+          )}
+        </div>
+        {section.events &&
+          <div className={styles.events}>
+            {events.map(event =>
+              <div className={styles.event} key={event.id}>
+                <div className={styles.time}>
+                  <div className={styles.range}>
+                    {moment.utc(event.started_at).format('HH:mm')}
+                     — 
+                    {moment.utc(event.ended_at).format('HH:mm')}
+                  </div>
+
+                  {event.book &&
+                    <div className={styles.book}>
+                      По записи
+                      {event.book_as_link &&
+                        <a href={event.book} target="_blank">Записаться</a>
+                      }
+                    </div>
+                  }
+                </div>
+                <div className={styles.content}>
+                  <div className={styles.title}>
+                    {event.title}
+                  </div>
+
+                  {/* <div className={styles.speakers}>
+                    {event.speakers.map(speaker => speaker.title).join(', ')}
+                  </div> */}
+
+                  {event.place &&
+                    <div className={styles.place}>
+                      {event.place.title_short || event.place.title}, {event.place.desc}
+                    </div>
+                  }
+
+                  {/* {event.desc &&
+                    <div className={styles.desc}>
+                      {event.desc}
+                    </div>
+                  } */}
+                </div>
+              </div>
+            )}
+          </div>
+        }
+
+        {/* {eventsCount > 2 &&
+          <div className={classNames(styles.more, { [styles.open]: this.state.open })} onClick={this.handleToggleClick}>
+            {this.state.open ? 'Свернуть' : 'Полное расписание'}
+          </div>
+        } */}
+      </div>
+    );
+  }
+}
+
+export default Section
